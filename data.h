08bb8bd549431219d6a491fc0cee2de0044d0a48
An2An <an2an96@bk.ru>
2015-05-31 01:48:37 +0300

    Create data.h

---
+++ data.h
@@ -0,0 +1,43 @@
+#ifndef NAMEINFO_H
+#define NAMEINFO_H
+
+#include <QString>
+
+struct FullName {
+    QString Surname;
+    QString Name;
+    QString Fathername;
+};
+
+class NameInfo  {
+public:
+    NameInfo(const QString& name);
+
+    void SetName(const QString&);
+    void SetSurname(const QString&);
+    void SetFathername_m(const QString&);
+    void SetFathername_f(const QString&);
+    void SetGender(const QChar&);
+
+    QString GetName();
+    QString GetSurname();
+    QString GetFathername_m();
+    QString GetFathername_f();
+    QChar GetGender();
+
+    static QString GenerateSurname(const QString&);
+    static QString GenerateFathername_m(const QString&);
+    static QString GenerateFathername_f(const QString&);
+    static QChar DetermineGender(const QString&source);
+
+private:
+    static QString ChangeFormat(const QString&, const char*, int = 0);
+
+    QString Name,
+            Surname,
+            Fathername_m,
+            Fathername_f;
+    QChar   Gender;
+};
+
+#endif // NAMEINFO_H
